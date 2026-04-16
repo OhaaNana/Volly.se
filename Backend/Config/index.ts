4f612ddcc11a1551a89fe483f586bd9d3b1e76b5
IIMaksymilianDymitrII <maksymiliandymitr@chasacademy.se>
2026-04-16 10:29:03 +0200

    corrected order of operations, added routes

--- Backend/Config/index.ts
+++ Backend/Config/index.ts
@@ -1,6 +1,5 @@
 import type { FastifyInstance } from "fastify";
 import pluginSetup from "./plugin";
-import setupErrorHandlers from "./errorHanders";
 import dbSetup from "./db";
 import websocketSetup from "./wb";
 
@@ -8,11 +7,4 @@ export default async function Setup(app:FastifyInstance) {
   await pluginSetup(app)
   await dbSetup(app)
   await websocketSetup(app)
-  
-  setupErrorHandlers(app, )
-  
-  app.listen({ port: 3001, host: "0.0.0.0" }, (address) =>
-     console.log(`Server is running at ${address}`),
-   );
- }
 }
