5a3730cd02bd53c5140897c299802111460f0a7d
IIMaksymilianDymitrII <maksymiliandymitr@chasacademy.se>
2026-04-16 10:17:47 +0200

    small adjustments

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,12 +1,9 @@
 import fastify, {type FastifyInstance } from "fastify";
-import Config from "./Config/plugin";
 import Setup from "./Config";
-
 
 function start() {
   const app = fastify({ logger: true })
   Setup(app)
-  
 }
 
 start()
