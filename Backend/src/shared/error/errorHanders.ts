c8afabd9c29d7b39ff50a16d0de111efbe55a283
IIMaksymilianDymitrII <maksymilian.dymitr@chasacademy.se>
2026-05-19 14:28:13 +0200

    fix: There was a part that belonged to specifically mongodb that we did not use

--- Backend/src/shared/error/errorHanders.ts
+++ Backend/src/shared/error/errorHanders.ts
@@ -3,8 +3,7 @@ import type { FastifyInstance } from "fastify/types/instance";
 
 type AppError = Error & {
   validation?: unknown;
-  code?: string;
-  path?: string;
+  code?: string; // PostgreSQL uses error codes like 23505
   statusCode?: number;
 };
 
@@ -29,26 +28,24 @@ export default function setupErrorHandlers(app: FastifyInstance) {
           details: error.validation,
         });
       }
-      if (error.code === "23505" || error.code === "11000") {
-        return reply.status(400).send({
+
+      if (error.code === "23505") {
+        return reply.status(409).send({
           error: "Conflict",
-          message: "This data already exists",
+          message: "This resource already exists",
         });
       }
-      if (error.name === "CastError") {
-        return reply.status(400).send({
-          error: "Invalid ID",
-          message: `The Provided ID is Incorrect: ${error.path}`,
-        });
-      }
+
       if (error.statusCode && error.statusCode < 500) {
         return reply.status(error.statusCode).send({
           error: error.name,
           message: error.message,
         });
       }
+
       console.log(error);
-      reply.status(500).send({
+
+      return reply.status(500).send({
         error: "Internal Server Error",
         message: "Something Went Wrong",
       });
@@ -56,8 +53,9 @@ export default function setupErrorHandlers(app: FastifyInstance) {
   );
 
   app.setNotFoundHandler((request: FastifyRequest, reply: FastifyReply) => {
-    console.log(`Route has not been found: ${request.method}:${request.url}`);
-    reply.status(404).send({
+    console.log(`Route not found: ${request.method}:${request.url}`);
+
+    return reply.status(404).send({
       error: "Not Found",
       message: `Route ${request.method}:${request.url} does not exist`,
     });
