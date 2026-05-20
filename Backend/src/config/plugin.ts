e6b94672b2aa2df614d175eb47e25283eed68477
simon-jmk <simonjmk@gmail.com>
2026-05-20 09:53:12 +0200

    Update swagger UI route prefix to /api/docs

--- Backend/src/config/plugin.ts
+++ Backend/src/config/plugin.ts
@@ -16,7 +16,7 @@ export default async function pluginSetup(app: FastifyInstance) {
   });
 
   await app.register(swaggerUi, {
-    routePrefix: "/docs",
+    routePrefix: "/api/docs",
   });
 
   app.register(fastifyCors, {
